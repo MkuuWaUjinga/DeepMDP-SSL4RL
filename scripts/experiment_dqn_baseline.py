c29104353a91e434ceabd7f57ccc49036b63f73d
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-04-13 12:30:26 +0200

    Push best lunar lander config

--- scripts/experiment_dqn_baseline.py
+++ scripts/experiment_dqn_baseline.py
@@ -5,27 +5,7 @@ from garage.experiment import deterministic
 from dowel import logger
 
 logger.add_output(dowel.StdOutput())
-deterministic.set_seed(1)
+deterministic.set_seed(21)
 ex.observers.append(FileStorageObserver("runs"))
-ex.add_config({
-    "env_name": "LunarLander-v2",
-    "dqn_config": {
-        "learning_rate": 0.001,
-        "replay_buffer_size": 500000,
-        "buffer_batch_size": 64,
-        "n_epochs": 20,
-        "net": {
-            "filter_dims": (),
-            "num_filters": (),
-            "strides": (),
-            "dense_sizes": (512, 256),
-            "input_shape": (8,)
-        },
-        "epsilon_greedy": {
-            "max_epsilon": 1.0,
-            "min_epsilon": 0.05,
-            "decay_ratio": 0.05,
-        }
-    }
-})
+ex.add_config("configs/lunar_lander.json")
 ex.run()
