c15707acec79752dde4a5ed1997c63032b911c96
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-04-25 21:54:50 +0200

    Add click and run script for lunar lander exp

--- scripts/experiment_dqn_baseline.py
+++ scripts/experiment_dqn_baseline.py
@@ -1,11 +1,18 @@
 import dowel
+import click
 from deepmdp.experiments.dqn_baseline import ex
 from sacred.observers import FileStorageObserver
 from garage.experiment import deterministic
 from dowel import logger
 
-logger.add_output(dowel.StdOutput())
-deterministic.set_seed(21)
-ex.observers.append(FileStorageObserver("runs"))
-ex.add_config("configs/LunarLander/image-obs/lunar_lander.json")
-ex.run()
+@click.command()
+@click.option('--config_path', help='Path for experiment\'s config')
+def start_exp(config_path):
+    logger.add_output(dowel.StdOutput())
+    deterministic.set_seed(21)
+    ex.observers.append(FileStorageObserver("runs"))
+    ex.add_config(config_path)
+    ex.run()
+
+if __name__=="__main__":
+    start_exp()
