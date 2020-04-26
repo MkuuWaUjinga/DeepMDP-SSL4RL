243f1ef5fcbba94026736d7dd850aa392ed3d552
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-04-26 18:17:13 +0200

    Remove unused code

--- deepmdp/garage_mod/env_wrappers/lunar_lander_to_image_obs.py
+++ deepmdp/garage_mod/env_wrappers/lunar_lander_to_image_obs.py
@@ -1,10 +1,7 @@
 import numpy as np
-import pyglet
-from pyglet.gl import *
 import gym
 import gym.spaces
-from gym.envs.classic_control.rendering import Viewer, Transform
-from gym.envs.box2d.lunar_lander import VIEWPORT_H, VIEWPORT_W, SCALE
+from gym.envs.box2d.lunar_lander import VIEWPORT_H, VIEWPORT_W
 
 class LunarLanderToImageObservations(gym.Wrapper):
     """Wrap for Lunar Lander environments. Turns LunarLander's semantically rich observation vector to RGB-image
@@ -38,65 +35,15 @@ class LunarLanderToImageObservations(gym.Wrapper):
     def observation_space(self, observation_space):
         self._observation_space = observation_space
 
-    def convert_to_image(self, obs):
-        # Inject custom viewer in Lunar Lander env that doesn't open a window.
-        #viewer = LunarLanderViewer(VIEWPORT_W, VIEWPORT_H)
-        #viewer.set_bounds(0, VIEWPORT_W / SCALE, 0, VIEWPORT_H / SCALE)
-        #env_viewer = self.env.env.viewer
-        #self.env.env.viewer = viewer
-        obs = self.env.render(mode="rgb_array")
-        #self.env.env.viewer = env_viewer
-        return obs
+    def render_image(self):
+        return self.env.render(mode="rgb_array")
 
-    def reset(self):
+    def reset(self) -> int:
         """gym.Env reset function."""
-        return self.convert_to_image(self.env.reset())
+        self.env.reset()
+        return self.render_image()
 
     def step(self, action):
         """gym.Env step function."""
-        obs, reward, done, info = self.env.step(action)
-        return self.convert_to_image(obs), reward, done, info
-
-class LunarLanderViewer(Viewer):
-
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-        self.geoms = []
-        self.onetime_geoms = []
-        self.transform = Transform()
-
-    def close(self):
-        pass
-
-    def render(self, return_rgb_array=False):
-        #window = pyglet.window.Window(width=self.width, height=self.height, visible=True)
-        #window.minimize()
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glClearColor(1, 1, 1, 1)
-        #window.clear()
-        #window.dispatch_events()
-        self.transform.enable()
-        for geom in self.geoms:
-            geom.render()
-        for geom in self.onetime_geoms:
-            geom.render()
-        self.transform.disable()
-        arr = None
-        if return_rgb_array:
-            buffer = pyglet.image.get_buffer_manager().get_color_buffer()
-            image_data = buffer.get_image_data()
-            arr = np.frombuffer(image_data.data, dtype=np.uint8)
-            # In https://github.com/openai/gym-http-api/issues/2, we
-            # discovered that someone using Xmonad on Arch was having
-            # a window of size 598 x 398, though a 600 x 400 window
-            # was requested. (Guess Xmonad was preserving a pixel for
-            # the boundary.) So we use the buffer height/width rather
-            # than the requested one.
-            arr = arr.reshape(buffer.height, buffer.width, 4)
-            arr = arr[::-1, :, 0:3]
-        #window.flip()
-        self.onetime_geoms = []
-        #window.close()
-        return arr
+        _, reward, done, info = self.env.step(action)
+        return self.render_image(), reward, done, info
