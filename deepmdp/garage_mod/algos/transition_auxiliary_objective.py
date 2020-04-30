94cb6caf7fe5601cb900ce463fff5e28cea0b58d
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-04-30 10:54:05 +0200

    Add method strings

--- deepmdp/garage_mod/algos/transition_auxiliary_objective.py
+++ deepmdp/garage_mod/algos/transition_auxiliary_objective.py
@@ -17,11 +17,12 @@ class TransitionAuxiliaryObjective(AuxiliaryObjective):
                  b_init=torch.nn.init.zeros_):
 
         self._env_spec = env_spec
+        self.in_channels = in_channels
 
         action_dim = self._env_spec.action_space.flat_dim
         conv = torch.nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=in_channels * action_dim,
+                in_channels=self.in_channels,
+                out_channels=self.in_channels * action_dim,
                 kernel_size=2,
                 stride=1,
             ).to(device)
@@ -30,12 +31,19 @@ class TransitionAuxiliaryObjective(AuxiliaryObjective):
         zero_pad = torch.nn.ZeroPad2d((0, 1, 0, 1)) # maintain input dimensionality.
         self.latent_transition_network = torch.nn.Sequential(zero_pad, conv, nonlinearity())
 
-    def compute_loss(self, embedding, transition, actions):
+    def compute_loss(self, embedding, embedding_next_observation, actions):
+        """
+        Compute loss between embedding of next observation and the predicted embedding of the next observation.
+        :param embedding: The embedded observation used as an input for the latent transition network
+        :param embedding_next_observation: The ground truth embedded next obervation
+        :param actions: The actions that caused the embedded next_observations.
+        :return: The mean squared error between the predicted and the ground truth embedding of the next observation.
+        """
         preds = self.latent_transition_network(embedding)
         mask = np.zeros((len(actions), preds.size()[1], preds.size()[2], preds.size()[3]))
         for i, act in enumerate(actions):
-            mask[i, 64*int(act.item()):64*(int(act.item())+1), :, :] = 1
+            mask[i, self.in_channels * int(act.item()): self.in_channels * (int(act.item()) + 1), :, :] = 1
         mask = torch.from_numpy(mask).bool()
-        predicted_transition_selected_by_action = torch.masked_select(preds, mask)
+        predicted_next_observation_embedding = torch.masked_select(preds, mask)
         loss_func = torch.nn.MSELoss()
-        return loss_func(predicted_transition_selected_by_action, transition.flatten())
+        return loss_func(predicted_next_observation_embedding, embedding_next_observation.flatten())
