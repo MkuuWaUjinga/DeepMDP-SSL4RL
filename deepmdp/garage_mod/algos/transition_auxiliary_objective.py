4cf9a04051dc073ddcaab500e0406a5e0db4a63e
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-05-12 17:13:53 +0200

    Extend transition network to have a different transition network as well

--- deepmdp/garage_mod/algos/transition_auxiliary_objective.py
+++ deepmdp/garage_mod/algos/transition_auxiliary_objective.py
@@ -1,35 +1,44 @@
-# Neural Network variante
-# Minimiere Wasserstein distanz zwischen DeepMDP.
-
 import torch
-import numpy as np
 
 from deepmdp.garage_mod.algos.auxiliary_objective import AuxiliaryObjective
+from garage.torch.modules.mlp_module import MLPModule
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class TransitionAuxiliaryObjective(AuxiliaryObjective):
 
     def __init__(self,
                  env_spec,
-                 in_channels,
+                 input_dim,
+                 use_linear_transition_net=False,
                  nonlinearity=torch.nn.ReLU,
                  w_init=torch.nn.init.xavier_normal_,
                  b_init=torch.nn.init.zeros_):
 
         self._env_spec = env_spec
-        self.in_channels = in_channels
+        self.input_dim = input_dim
         self.action_dim = self._env_spec.action_space.flat_dim
-
-        conv = torch.nn.Conv2d(
-                in_channels=self.in_channels,
-                out_channels=self.in_channels * self.action_dim,
-                kernel_size=2,
-                stride=1,
-            ).to(device)
-        w_init(conv.weight)
-        b_init(conv.bias)
-        zero_pad = torch.nn.ZeroPad2d((0, 1, 0, 1)) # maintain input dimensionality.
-        self.latent_transition_network = torch.nn.Sequential(zero_pad, conv, nonlinearity())
+        if not use_linear_transition_net:
+            conv = torch.nn.Conv2d(
+                    in_channels=self.input_dim,
+                    out_channels=self.input_dim * self.action_dim,
+                    kernel_size=2,
+                    stride=1,
+                ).to(device)
+            w_init(conv.weight)
+            b_init(conv.bias)
+            zero_pad = torch.nn.ZeroPad2d((0, 1, 0, 1)) # maintain input dimensionality.
+            self.latent_transition_network = torch.nn.Sequential(zero_pad, conv, nonlinearity())
+        else:
+            self.latent_transition_network = MLPModule(input_dim=self.input_dim,
+                             output_dim=self.input_dim * self.action_dim,
+                             hidden_sizes=[512],
+                             hidden_nonlinearity=torch.nn.ReLU,
+                             hidden_w_init=w_init,
+                             hidden_b_init=b_init,
+                             output_nonlinearity=None,
+                             output_w_init=w_init,
+                             output_b_init=b_init)
 
     def compute_loss(self, embedding, embedding_next_observation, actions):
         """
@@ -42,12 +51,12 @@ class TransitionAuxiliaryObjective(AuxiliaryObjective):
         preds = self.latent_transition_network(embedding)
         batch_size = actions.size(0)
         # Reshape tensor: B x act * channels ... --> B x channels x ... x act
-        preds = preds.unsqueeze(len(preds.size())).reshape(batch_size, self.in_channels, *preds.size()[2:4], self.action_dim)
+        preds = preds.unsqueeze(len(preds.size())).reshape(batch_size, self.input_dim, *preds.size()[2:4], self.action_dim)
         loss_func = torch.nn.MSELoss()
         loss = 0
         for i, act in enumerate(actions):
-            predicted__next_observiation_embedding = preds[i, ..., int(act.item())].squeeze()
+            predicted__next_observation_embedding = preds[i, ..., int(act.item())].squeeze()
             ground_truth_embedding = embedding_next_observation[i, ...]
-            assert(ground_truth_embedding.size() == predicted__next_observiation_embedding.size())
-            loss += loss_func(predicted__next_observiation_embedding, ground_truth_embedding)
+            assert(ground_truth_embedding.size() == predicted__next_observation_embedding.size())
+            loss += loss_func(predicted__next_observation_embedding, ground_truth_embedding)
         return loss
