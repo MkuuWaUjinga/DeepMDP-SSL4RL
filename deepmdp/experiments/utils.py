fc152642d31255fa76fbb0ee2286929bbde165fd
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-05-24 12:48:05 +0200

    Extend visualization capabilites: Plotting of correlation heatmap

--- deepmdp/experiments/utils.py
+++ deepmdp/experiments/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from visdom import Visdom
 from collections import defaultdict
+import torch
+
 
 def show_agent_playing(policy, env):
     obs = env.reset()
@@ -14,17 +16,75 @@ def show_agent_playing(policy, env):
         env.render()
 
 
-class VisdomLinePlotter(object):
+class Visualizer:
+
+    def __init__(self, experiment_id, port=9098):
+        self.port = 9098
+        self.viz = Visdom(port=port)
+        self.env = experiment_id
+        self.line_plotter = VisdomLinePlotter(self.viz, env_name=experiment_id)
+        self.correlation_plot_window = None
+
+    def visualize_latent_space_correlation(self, embedding, ground_truth_embedding):
+        assert embedding.size() == ground_truth_embedding.size()
+        # Calculate correlation
+        correlation_matrix = self.calculate_correlation(embedding.t(), ground_truth_embedding.t())
+        self.correlation_plot_window = self.viz.heatmap(X=torch.abs(correlation_matrix),
+                                                        env=self.env,
+                                                        win=self.correlation_plot_window,
+                                                        opts=dict(
+                                                            columnnames=["pos_x", "pos_y", "vel_x", "vel_y", "ang",
+                                                                         "ang_vel", "leg_1", "leg_2"],
+                                                            rownames=['l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8'],
+                                                            colormap='Viridis',
+                                                            xmin=0,
+                                                            xmax=1.0
+                                                        ))
+
+    def calculate_correlation(self, x1, x2):
+        """
+        takes two 2D tensors of (latent_space_size, sample_size) and calculates the column-wise correlation between the
+        two
+        :param x1:
+        :param x2:
+        :return: a 2D tensor of shape (latent_space_size, latent_space_size)
+        """
+        with torch.no_grad():
+            # Calculate covariance matrix of columns
+            mean_x1 = torch.mean(x1, 1).unsqueeze(1)
+            mean_x2 = torch.mean(x2, 1).unsqueeze(1)
+            x1m = x1.sub(mean_x1)
+            x2m = x2.sub(mean_x2)
+            c = x1m.mm(x2m.t())
+            c = c / (x1.size(1) - 1)
+            # Normalize by standard deviations. Add epsilon for numerical stability if std close to 0
+            epsilon = 1e-9
+            std_x1 = torch.std(x1, 1).unsqueeze(1) + epsilon
+            std_x2 = torch.std(x2, 1).unsqueeze(1) + epsilon
+            c = c.div(std_x1)
+            c = c.div(std_x2.t())
+
+            assert torch.max(c).item() <= 1.0 and torch.min(
+                c).item() >= -1.0, "Invalid value for correlation coefficient!"
+            return c
+
+    def plot(self, var_name, split_name, title_name, x, y, color=np.array([[255, 136, 0], ])):
+        xlabel = "episode number"
+        self.line_plotter.xlabel = xlabel
+        self.line_plotter.plot(var_name, split_name, title_name, x, y, color)
+
+
+class VisdomLinePlotter:
     """Plots to Visdom"""
 
-    def __init__(self, port, env_name='main', xlabel='Iteration'):
-        self.viz = Visdom(port=port)
+    def __init__(self, viz, env_name='main', xlabel='Iteration'):
+        self.viz = viz
         self.env = env_name
         self.plots = {}
         self.legend = defaultdict(dict)
         self.xlabel = xlabel
 
-    def plot(self, var_name, split_name, title_name, x, y, color=np.array([[255, 136, 0],])):
+    def plot(self, var_name, split_name, title_name, x, y, color):
         self.legend[var_name][split_name] = None
         if var_name not in self.plots:
             self.plots[var_name] = self.viz.line(X=np.array([x, x]), Y=np.array([y, y]), env=self.env, opts=dict(
