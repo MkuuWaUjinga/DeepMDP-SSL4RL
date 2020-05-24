Fix gradient penalty computation

deepmdp/garage_mod/algos/dqn.py
@@ -127,15 +127,14 @@ class DQN(OffPolicyRLAlgorithm):
                 _, embedding = self.qf(next_observations, return_embedding=True)
                 self.visualizer.save_latent_space(embedding, transitions["ground_truth_state"])
 
-        """
+
         # compute gradient penalty if we have auxiliary objectives i.e. we train a DeepMDP
         if self.auxiliary_objectives:
             gradient_penalty = 0
-            gradient_penalty += self.compute_gradient_penalty(observations, self.qf.encoder)
-            for head in [self.qf.head] + self.auxiliary_objectives:
+            gradient_penalty += self.compute_gradient_penalty(self.qf.encoder, observations)
+            for head in [self.qf.head] + [aux.net for aux in self.auxiliary_objectives]:
                 gradient_penalty += self.compute_gradient_penalty(head, embedding)
-                loss += self.penalty_lambda * gradient_penalty
-        """
+            loss += self.penalty_lambda * gradient_penalty
 
         loss_func = torch.nn.SmoothL1Loss()
         qval_loss = loss_func(q_selected, target)
@@ -160,7 +159,7 @@ class DQN(OffPolicyRLAlgorithm):
         paths = self.process_samples(itr, paths)
         epoch = itr / self.n_epoch_cycles
 
-        # todo log correlation between reward and q-value to see whether the agent's estimation of value was correct.
+        # ? log correlation between reward and q-value to see whether the agent's estimation of value was correct.
         self.episode_rewards.extend(paths['undiscounted_returns'])
         self.episode_mean_q_vals.extend(paths['episode_mean_q_vals'])
         self.episode_std_q_vals.extend(paths['episode_std_q_vals'])
@@ -258,13 +257,13 @@ class DQN(OffPolicyRLAlgorithm):
         https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/wgan_gp/wgan_gp.py"""
         # Random weight term for interpolation between real and fake samples
         batch_size = samples.size(0)
-        samples_a, samples_b = torch.split(samples, int(batch_size/2), dim=1)
+        samples_a, samples_b = torch.split(samples, int(batch_size/2))
         alpha = torch.rand_like(samples_a)
         # Get random interpolation between real and fake samples
         interpolated_obs = (samples_a * alpha + ((1.0 - alpha) * samples_b)).requires_grad_(True)
 
         d_interpolates = net(interpolated_obs)
-        grad = torch.ones(samples_b.shape[0], 1, requires_grad=False)
+        grad = torch.ones(d_interpolates.size(), requires_grad=False).to(device)
 
         # Get gradient w.r.t. interpolates
         gradients = torch.autograd.grad(
@@ -276,13 +275,7 @@ class DQN(OffPolicyRLAlgorithm):
             only_inputs=True,
         )[0]
 
-        penalty = 0
-        for grads in gradients:
-            grads = grads.view(grads.size(0), -1)
-            # We want the gradients to be close to 0
-            diff = grads.norm(2, dim=1)
-            if LP:
-                penalty = penalty + (torch.max(diff, torch.zeros_like(diff)) ** 2).mean()
-            else:
-                penalty = penalty + (diff ** 2).mean()
+        gradients = gradients.view(int(batch_size/2), -1)
+        gradients_norm = gradients.norm(2, dim=1)
+        penalty = (gradients_norm ** 2).mean()
         return penalty

deepmdp/garage_mod/algos/reward_auxiliary_objective.py
@@ -21,7 +21,7 @@ class RewardAuxiliaryObjective(AuxiliaryObjective):
 
         action_dim = self._env_spec.action_space.flat_dim
 
-        self.reward_network = MLPModule(
+        self.net = MLPModule(
             input_dim=embedding_dim,
             output_dim=action_dim,
             hidden_sizes=[],
@@ -33,7 +33,7 @@ class RewardAuxiliaryObjective(AuxiliaryObjective):
 
     def compute_loss(self, embedding, rewards, actions):
         assert list(actions.size()) == [32, 4]
-        preds = self.reward_network(embedding)
+        preds = self.net(embedding)
         selected_predicted_rewards = torch.sum(preds * actions, axis = 1)
         loss_func = torch.nn.SmoothL1Loss()
         return loss_func(selected_predicted_rewards, rewards)

deepmdp/garage_mod/algos/transition_auxiliary_objective.py
@@ -28,17 +28,17 @@ class TransitionAuxiliaryObjective(AuxiliaryObjective):
             w_init(conv.weight)
             b_init(conv.bias)
             zero_pad = torch.nn.ZeroPad2d((0, 1, 0, 1)) # maintain input dimensionality.
-            self.latent_transition_network = torch.nn.Sequential(zero_pad, conv, nonlinearity())
+            self.net = torch.nn.Sequential(zero_pad, conv, nonlinearity())
         else:
-            self.latent_transition_network = MLPModule(input_dim=self.input_dim,
-                             output_dim=self.input_dim * self.action_dim,
-                             hidden_sizes=[512],
-                             hidden_nonlinearity=torch.nn.ReLU,
-                             hidden_w_init=w_init,
-                             hidden_b_init=b_init,
-                             output_nonlinearity=None,
-                             output_w_init=w_init,
-                             output_b_init=b_init)
+            self.net = MLPModule(input_dim=self.input_dim,
+                                 output_dim=self.input_dim * self.action_dim,
+                                 hidden_sizes=[512],
+                                 hidden_nonlinearity=torch.nn.ReLU,
+                                 hidden_w_init=w_init,
+                                 hidden_b_init=b_init,
+                                 output_nonlinearity=None,
+                                 output_w_init=w_init,
+                                 output_b_init=b_init)
 
     def compute_loss(self, embedding, embedding_next_observation, actions):
         """
@@ -48,7 +48,7 @@ class TransitionAuxiliaryObjective(AuxiliaryObjective):
         :param actions: The actions that caused the embedded next_observations.
         :return: The mean squared error between the predicted and the ground truth embedding of the next observation.
         """
-        preds = self.latent_transition_network(embedding)
+        preds = self.net(embedding)
         batch_size = actions.size(0)
         # Reshape tensor: B x act * channels ... --> B x channels x ... x act
         preds = preds.unsqueeze(len(preds.size())).reshape(batch_size, self.input_dim, *preds.size()[2:4], self.action_dim)