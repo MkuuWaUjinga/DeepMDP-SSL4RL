24478ccc21d282bc73e81e541db1eafefb745aaa
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-05-27 10:54:28 +0200

    Remove visdom server starting from script

--- submit.sh
+++ submit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #SBATCH --job-name=deepmdp-repro
 srun hostname
-srun visdom -port 9098
-srun python deepmdp-repro/scripts/experiment_dqn_baseline.py
+srun python experiment_dqn_baseline.py
