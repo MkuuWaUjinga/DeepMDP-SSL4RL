52f613076958e5a2bbe04f52a6d069b0c68912f9
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-05-27 11:51:48 +0200

    try again

--- submit.sh
+++ submit.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=deepmdp-repro
 #SBATCH --time=05:00:00
 #SBATCH --ntasks=8
-#SBATCH --partition=simba-compute-18
+#SBATCH --partition=simba*
+#SBATCH --nodelist=simba-compute-18
 srun hostname
 srun python scripts/experiment_dqn_baseline.py
