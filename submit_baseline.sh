a188029c92aa7b5d38baa88ecef28019c964456d
Adrian Ziegler <aziegler@simba-fe.iel.epfl.ch>
2020-06-16 14:59:27 +0200

    Add submit script for dqn baseline

---
+++ submit_baseline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=deepmdp-repro
+#SBATCH --time=0
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=gpu
+#SBATCH --nodelist=simba-compute-gpu-2
+srun hostname
+srun python scripts/experiment_dqn_baseline.py --config_path scripts/configs/LunarLander/scalar-obs/baseline-new-architecture/lunar_lander.json 
