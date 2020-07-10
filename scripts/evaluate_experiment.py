004c7343d6d96dd2c022a3469c2252334007af98
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-07-10 15:13:30 +0200

    Add method to evaluate of learned latent space

--- scripts/evaluate_experiment.py
+++ scripts/evaluate_experiment.py
@@ -1,10 +1,31 @@
 import pickle
+import torch
 from deepmdp.garage_mod.policies.discrete_qf_derived_policy import DiscreteQfDerivedPolicy
 from deepmdp.experiments.dqn_baseline import setup_stacked_lunar_lander_env
 import numpy as np
 from gym.utils.play import play
-import matplotlib; matplotlib.use('TkAgg')
+from visdom import Visdom
+import matplotlib;
+
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import gym
+from garage.envs import GarageEnv
+
+lunar_lander_key_map = {(ord('w'),): 2,
+                        (ord('s'),): 0,
+                        (ord('d'),): 3,
+                        (ord('a'),): 1}
+
+# Quantiles:
+quantiles = np.array([[-0.84, 0.83],
+             [-0.12, 1.6],
+             [-1.38, 1.40],
+             [-1.57, 0.36],
+             [-1.41, 1.39],
+             [-1.08, 0.89],
+             [0, 1],
+                      [0, 1]], dtype=np.float32)
 
 def evaluate_trained_policy(env):
     with open("configs/LunarLander/scalar-obs/baseline-new-architecture/deepmdp_baseline.pkl", "rb") as file:
@@ -34,6 +55,7 @@ def evaluate_trained_policy(env):
 def play_env(setup_env, key_mapping=None):
     play(setup_env, keys_to_action=key_mapping)
 
+
 def random_policy(env):
     max = 0
     min = 0
@@ -42,6 +64,7 @@ def random_policy(env):
         rewards = []
         done = False
         while not done:
+            env.render()
             act = env.action_space.sample()
             obs, reward, done, _ = env.step(act)
             rewards.append(reward)
@@ -49,21 +72,102 @@ def random_policy(env):
                 max = np.max(obs)
             if np.min(obs) < min:
                 min = np.min(obs)
-        #print(f"summed reward for this episode is {np.sum(np.array(rewards))}")
+        # print(f"summed reward for this episode is {np.sum(np.array(rewards))}")
     print(f"min {min} max {max}")
+
 
 def show_lunar_obs(obs):
     obs = np.squeeze(obs)
     plt.imshow(obs, cmap='gray', vmin=0, vmax=255)
     plt.show()
 
-lunar_lander_key_map = {(ord('w'), ): 2,
-                   (ord('s'), ): 0,
-                   (ord('d'), ): 3,
-                   (ord('a'), ): 1}
 
-if __name__=="__main__":
+def normalize(vector):
+    tiled_1th_quantile = np.tile(quantiles[:, 0], (num_stacked_frames, 1))
+    tiled_99th_quantile = np.tile(quantiles[:, 1], (num_stacked_frames, 1))
+    reshaped_normalized = (vector.reshape(8, 4).transpose() - tiled_1th_quantile) / (tiled_99th_quantile - tiled_1th_quantile)
+    assert np.min(reshaped_normalized) >= 0 and np.max(reshaped_normalized) <= 1
+    return reshaped_normalized.transpose().flatten()
+
+def project_back(vector):
+    tiled_1th_quantile = np.tile(quantiles[:, 0], (num_stacked_frames, 1))
+    tiled_99th_quantile = np.tile(quantiles[:, 1], (num_stacked_frames, 1))
+    reshaped_normalized = vector.reshape(8, 4).transpose() * (tiled_99th_quantile - tiled_1th_quantile) + tiled_1th_quantile
+    return reshaped_normalized.transpose().flatten()
+
+def latent_space_eval(obs, model_name, perturbed_index):
+    with open(f"configs/LunarLander/scalar-obs/baseline-new-architecture/{model_name}.pkl", "rb") as file:
+        exp = pickle.load(file)
+    algo = exp['algo']
+    q_net = algo.qf
+    q_net.eval()
+    viz = Visdom(port=9098)
+    num_perturbations = 100
+    l1_obs_list = []
+    l1_embedding_list = []
+
+    # Just for testing
+    normalized_obs = normalize(obs)
+    back_projected = project_back(normalized_obs)
+    assert np.allclose(obs, back_projected, rtol=1e-6, atol=1e-6)
+
+    with torch.no_grad():
+        _, embedding = q_net(torch.Tensor(obs).unsqueeze(0), return_embedding=True)
+
+    for j in range(num_perturbations):
+        # perturb all occurences of attribute of index perturbed_index [-1, 1]
+        normalized_obs = normalize(obs)
+        perturbed_obs = np.copy(normalized_obs)
+        perturbed_obs[perturbed_index*num_stacked_frames:(perturbed_index+1) * num_stacked_frames] = j / num_perturbations # TODO try out only perturbing the latest obs of attribute
+        obs_delta = normalized_obs - perturbed_obs
+
+        back_projected_perturbed_obs = project_back(perturbed_obs)
+        with torch.no_grad():
+            _, perturbed_embedding = q_net(torch.Tensor(back_projected_perturbed_obs).unsqueeze(0), return_embedding=True)
+        embedding_delta = embedding - perturbed_embedding
+        l1_obs_delta = np.linalg.norm(obs_delta, ord=1)
+        l1_embedding_delta = np.linalg.norm(embedding_delta.numpy(), ord=1)
+        l1_obs_list.append(l1_obs_delta.item())
+        l1_embedding_list.append(l1_embedding_delta.item())
+
+    x = np.stack((np.array(l1_obs_list), np.array(l1_embedding_list)), axis=1)
+    viz.scatter(
+        X=x
+    )
+
+
+def determine_min_max(env, index):
+    k = 100000
+    done = True
+    data = []
+    for _ in range(k):
+        if done:
+            env.reset()
+        act = env.action_space.sample()
+        obs, _, done, env_info = env.step(act)
+        val = obs[index]
+        data.append(val)
+    print(f"quantile 0.99 {np.quantile(np.array(data), 0.99)}")
+    print(f"quantile 0.01 {np.quantile(np.array(data), 0.01)}")
+
+
+if __name__ == "__main__":
+    seed = 4
+    num_stacked_frames = 4
     number_of_episodes = 100
-    env = setup_stacked_lunar_lander_env("LunarLander-v2", 4)
-    #play_env(env, lunar_lander_key_map)
-    evaluate_trained_policy(env)
+    env = setup_stacked_lunar_lander_env("LunarLander-v2", num_stacked_frames)
+    env.seed(seed)
+    env.action_space.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    model_name_deepmdp = "deepmdp_baseline"
+    model_name_vanilla = "vanilla_dqn_baseline_latent"
+
+    # play_env(env, lunar_lander_key_map)
+    obs = env.reset()
+    for _ in range(4):
+        obs, _, _, _ = env.step(0)
+
+    for i in range(6):
+        latent_space_eval(obs, model_name_deepmdp, i)
+
