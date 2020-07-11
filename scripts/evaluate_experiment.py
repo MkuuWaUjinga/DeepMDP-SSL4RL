030b6b92cfe471ea3f3e100c251961412e6857d6
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-07-11 11:56:46 +0200

    Add experiment to evaluate latent space along trajectory

--- scripts/evaluate_experiment.py
+++ scripts/evaluate_experiment.py
@@ -6,7 +6,7 @@ import numpy as np
 from gym.utils.play import play
 from visdom import Visdom
 from deepmdp.experiments.utils import VisdomLinePlotter
-import matplotlib;
+import matplotlib
 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -96,8 +96,8 @@ def project_back(vector):
     reshaped_normalized = vector.reshape(8, 4).transpose() * (tiled_99th_quantile - tiled_1th_quantile) + tiled_1th_quantile
     return reshaped_normalized.transpose().flatten()
 
-def latent_space_eval(obs, model_name, perturbed_index, viz):
-    with open(f"configs/LunarLander/scalar-obs/baseline-new-architecture/{model_name}.pkl", "rb") as file:
+def latent_space_eval(obs, model_name, num_steps, perturbed_index, viz):
+    with open(f"configs/LunarLander/scalar-obs/baseline-new-architecture/{model_name_to_path[model_name]}.pkl", "rb") as file:
         exp = pickle.load(file)
     algo = exp['algo']
     q_net = algo.qf
@@ -105,7 +105,7 @@ def latent_space_eval(obs, model_name, perturbed_index, viz):
     num_perturbations = 100
     l1_obs_list = []
     l1_embedding_list = []
-    obs[perturbed_index*num_stacked_frames:(perturbed_index+1)*num_stacked_frames] = quantiles[perturbed_index][1]
+    #obs[perturbed_index*num_stacked_frames:(perturbed_index+1)*num_stacked_frames] = quantiles[perturbed_index][0]
 
     # Just for testing
     normalized_obs = normalize(obs)
@@ -119,33 +119,69 @@ def latent_space_eval(obs, model_name, perturbed_index, viz):
         # perturb all occurences of attribute of index perturbed_index [-1, 1]
         normalized_obs = normalize(obs)
         perturbed_obs = np.copy(normalized_obs)
-        perturbed_obs[perturbed_index*num_stacked_frames+3] = j/num_perturbations # Only perturb lates argument
-        obs_delta = normalized_obs - perturbed_obs
+        new_val = 1*(j/num_perturbations)
+        perturbed_obs[perturbed_index*num_stacked_frames+3] = new_val # Only perturb latest obs of argument
+        obs_delta = perturbed_obs - normalized_obs
 
         back_projected_perturbed_obs = project_back(perturbed_obs)
         with torch.no_grad():
             _, perturbed_embedding = q_net(torch.Tensor(back_projected_perturbed_obs).unsqueeze(0), return_embedding=True)
         embedding_delta = embedding - perturbed_embedding
-        l1_obs_delta = np.linalg.norm(obs_delta, ord=1)
+        #l1_obs_delta = epsilon - normalized_obs[perturbed_index * num_stacked_frames + 3]
+        epsilon = np.sum(obs_delta)
         l1_embedding_delta = np.linalg.norm(embedding_delta.numpy(), ord=1)
-        l1_obs_list.append(l1_obs_delta.item())
-        l1_embedding_list.append(l1_embedding_delta.item())
+        l1_obs_list.append(epsilon)
+        l1_embedding_list.append(l1_embedding_delta)
 
     x = np.stack((np.array(l1_obs_list), np.array(l1_embedding_list)), axis=1)
     x = x[x[:, 0].argsort()]
     for i in range(len(x)):
-        viz.plot("l1 in embedding space", str(perturbed_index), "Latent Space Exp", x[i, 0], x[i, 1], color=np.array([[(int(255/6))*perturbed_index, (int(255/6))*(6-perturbed_index), 0], ]))
-    #viz.line(
-    #    X=x[:, 0],
-    #    Y=x[:, 1],
-    #    opts=dict(
-    #        legend=list(range(6)),
-    #       linecolor=np.array([[(int(255/6))*perturbed_index, (int(255/6))*(6-perturbed_index), 0], ])
-    #    )
-    #)
+        viz.plot("l1 in embedding space", str(perturbed_index), f"Latent Space Exp {model_name} steps {num_steps}", x[i, 0], x[i, 1],
+                 color=np.array([[(int(255/6))*perturbed_index, (int(255/6))*(6-perturbed_index), 0], ]))
 
+def trajecory_interpolation(env, num_steps, perturbed_index, model_name, viz):
+    with open(f"configs/LunarLander/scalar-obs/baseline-new-architecture/{model_name_to_path[model_name]}.pkl", "rb") as file:
+        exp = pickle.load(file)
+    algo = exp['algo']
+    q_net = algo.qf
+    q_net.eval()
+    env.reset()
+    obss= []
+    for step in range(num_stacked_frames):
+        _ = env.step(0)
+    for step in range(num_stacked_frames, num_steps + num_stacked_frames):
+        obs, _, _, _ = env.step(0)
+        obss.append(obs)
 
-def determine_min_max(env, index):
+    # Just for testing
+    normalized_obs_test = normalize(obss[0])
+    back_projected = project_back(normalized_obs_test)
+    assert np.allclose(obss[0], back_projected, rtol=1e-6, atol=1e-6)
+
+    for i, obs in enumerate(obss):
+        with torch.no_grad():
+            _, embedding = q_net(torch.Tensor(obs).unsqueeze(0), return_embedding=True)
+
+        normalized_obs = normalize(obs)
+        perturbed_obs = np.copy(normalized_obs)
+        epsilon = -0.1
+        perturbed_obs[perturbed_index*num_stacked_frames+3] += epsilon
+        back_projected_perturbed_obs = project_back(perturbed_obs)
+        with torch.no_grad():
+            _, perturbed_embedding = q_net(torch.Tensor(back_projected_perturbed_obs).unsqueeze(0), return_embedding=True)
+        embedding_delta = embedding - perturbed_embedding
+        l1_embedding_delta = np.linalg.norm(embedding_delta.numpy(), ord=1)
+        viz.plot("l1 in embedding space", str(perturbed_index), f"Latent Space Exp {model_name} epsilon {epsilon}",
+                 i + num_stacked_frames, l1_embedding_delta,
+                 color=np.array([[(int(255/6))*perturbed_index, (int(255/6))*(6-perturbed_index), 0], ]))
+
+def determine_quantiles(env, index):
+    """
+    Calculates the quantiles of attribute with :param index in the input space. Works best with an unmodified Lunar Lander env.
+    :param env: The plain Lunar Lander env "LunarLander-v2"
+    :param index: The index of the attribute
+    :return:
+    """
     k = 100000
     done = True
     data = []
@@ -169,18 +205,27 @@ if __name__ == "__main__":
     env.action_space.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
-    model_name_deepmdp = "deepmdp_baseline"
-    model_name_vanilla = "vanilla_dqn_baseline_latent"
+    model_name_to_path = {
+       "deepmdp": "deepmdp_baseline",
+        "dqn": "vanilla_dqn_baseline_latent"
+    }
 
     # play_env(env, lunar_lander_key_map)
 
-    viz = Visdom(port=9098)
-    plotter = VisdomLinePlotter(viz, xlabel="l1 in observation space")
+    #  Trajectory interpolation
+    #plotter = VisdomLinePlotter(Visdom(port=9098), xlabel="num steps")
+    #for i in range(6):
+    #    print(i)
+    #    trajecory_interpolation(env, 40, i,"deepmdp" , plotter)
+
+    # Latent Space perturbation
+    plotter = VisdomLinePlotter(Visdom(port=9098), xlabel="epsilon in observation space")
     obs = env.reset()
-    for _ in range(4):
+    num_steps = 4
+    for _ in range(num_steps):
         obs, _, _, _ = env.step(0)
         env.render()
 
     for i in range(6):
-        latent_space_eval(obs, model_name_vanilla, i, plotter)
+        latent_space_eval(obs, "dqn", num_steps, i, plotter)
 
