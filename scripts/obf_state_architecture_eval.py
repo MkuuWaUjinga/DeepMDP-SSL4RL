b1d26b81c9ecdb5cb7d29863d2bc24acf6dda7a7
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2020-07-12 18:00:28 +0200

    Clean up architecture search script

--- scripts/obf_state_architecture_eval.py
+++ scripts/obf_state_architecture_eval.py
@@ -81,14 +81,12 @@ class ConvEncoderModel(torch.nn.Module):
                     ),
             torch.nn.Flatten()
        )
-        self.pool = torch.nn.MaxPool1d(8)
-        #self.lin = torch.nn.Linear(5, 3)
         self.lin_end = torch.nn.Sequential(
             torch.nn.LayerNorm(8*5),
             torch.nn.Linear(8*5, 64),
             torch.nn.ReLU(),
             torch.nn.LayerNorm(64),
-            torch.nn.Linear(64, 3)
+            torch.nn.Linear(64, 8)
         )
 
         self.conv.apply(self._init_weights)
@@ -97,12 +95,6 @@ class ConvEncoderModel(torch.nn.Module):
 
     def forward(self, x):
         map = self.conv(x)
-        last_obs = x.flatten(1)[:, num_stacked_obs-1::num_stacked_obs]
-        #skip = self.input_projection(last_obs)
-        #pooled = self.pool(map.permute(0, 2, 1)).squeeze()
-        #pooled = self.lin(pooled)
-        #down_project = self.lin(pooled)
-        #catted = torch.cat((down_project, last_obs), dim=1)
         return self.lin_end(map)
 
     @staticmethod
@@ -151,7 +143,7 @@ def train(model_name, reshape=False):
     model = model_map[model_name]
     print(model)
     batch_size = 32
-    num_epochs = 200
+    num_epochs = 1000
     train_print_progress_every = 500
     evaluate_only_obf_states = False
     print(f"Evaluating only obfuscated states {evaluate_only_obf_states}")
@@ -199,7 +191,7 @@ num_stacked_obs = 4
 num_unobf_states = 5
 torch.manual_seed(32)
 np.random.seed(32)
-#sample_transitions(500000, num_stacked_obs)
+sample_transitions(50000, num_stacked_obs)
 
 # Init regressors
 fc_regressor_64 = MLPModule(
@@ -212,23 +204,8 @@ fc_regressor_64 = MLPModule(
     output_nonlinearity=None,
     output_w_init=torch.nn.init.xavier_normal_,
     output_b_init=torch.nn.init.zeros_,
-    layer_normalization=False,
-    output_normalization=False
-)
-
-# Init regressors
-fc_regressor_32 = MLPModule(
-    input_dim=num_stacked_obs * num_unobf_states,
-    output_dim=8,
-    hidden_sizes=[32],
-    hidden_nonlinearity=torch.nn.ReLU,
-    hidden_w_init=torch.nn.init.xavier_normal_,
-    hidden_b_init=torch.nn.init.zeros_,
-    output_nonlinearity=None,
-    output_w_init=torch.nn.init.xavier_normal_,
-    output_b_init=torch.nn.init.zeros_,
-    layer_normalization=False,
-    output_normalization=False
+    layer_normalization="batch",
+    output_normalization=True
 )
 
 # Init regressors
@@ -256,27 +233,8 @@ fc_regressor_128_128 = MLPModule(
     output_nonlinearity=None,
     output_w_init=torch.nn.init.xavier_normal_,
     output_b_init=torch.nn.init.zeros_,
-    layer_normalization=False,
-    output_normalization=False
-)
-
-conv_regressor = torch.nn.Sequential(
-    torch.nn.Conv1d(
-                in_channels=num_stacked_obs,
-                out_channels=16,
-                kernel_size=1,
-                stride=1
-            ),
-    torch.nn.ReLU(),
-    torch.nn.Conv1d(
-                in_channels=16,
-                out_channels=32,
-                kernel_size=1,
-                stride=1
-            ),
-    torch.nn.AvgPool1d(1),
-    torch.nn.Flatten(),
-    torch.nn.Linear(32 * num_unobf_states, 8)
+    layer_normalization="batch",
+    output_normalization=True
 )
 
 conv_regressor = ConvEncoderModel()
@@ -285,8 +243,7 @@ model_map = {
     "fc64": fc_regressor_64,
     "fc128_128": fc_regressor_128_128,
     "fc64_64": fc_regressor_64_64,
-    #"fc32": fc_regressor_32,
-    #"conv": conv_regressor,
+    "conv": conv_regressor,
 }
 
 viz = Visdom(port=9098)
@@ -294,131 +251,3 @@ plotter = VisdomLinePlotter(viz, env_name="arch")
 
 for model_name in model_map:
    train(model_name)
-
-#train(fc_regressor)
-
-### Validation on obfuscated states
-#Epoch 8 | Val Loss 0.0360698327422142
-#Epoch 9 | Batch 0/9375 | Train Loss 0.016800491139292717
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.0321357436478138
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.03152284026145935
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.031437065452337265
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.03195047751069069
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.031601566821336746
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.031458258628845215
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.03130017966032028
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.031353551894426346
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.0313989631831646
-#Epoch 9 | Val Loss 0.03387722000479698
-
-#Epoch 48 | Val Loss 0.01563306525349617
-#Epoch 49 | Batch 0/9375 | Train Loss 0.0006666170083917677
-#Epoch 49 | Batch 1000/9375 | Train Loss 0.014545459300279617
-#Epoch 49 | Batch 2000/9375 | Train Loss 0.01447675097733736
-#Epoch 49 | Batch 3000/9375 | Train Loss 0.01461124513298273
-#Epoch 49 | Batch 4000/9375 | Train Loss 0.014168239198625088
-#Epoch 49 | Batch 5000/9375 | Train Loss 0.013844992965459824
-#Epoch 49 | Batch 6000/9375 | Train Loss 0.013732480816543102
-#Epoch 49 | Batch 7000/9375 | Train Loss 0.013524424284696579
-#Epoch 49 | Batch 8000/9375 | Train Loss 0.01342287939041853
-#Epoch 49 | Batch 9000/9375 | Train Loss 0.013385859318077564
-#Epoch 49 | Val Loss 0.015088058076798916
-
-### Validation calculated on all states
-#Epoch 8 | Val Loss 0.014309137128293514
-#Epoch 9 | Batch 0/9375 | Train Loss 0.003871660679578781
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.012485149316489697
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.012422861531376839
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.01215127483010292
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.012166060507297516
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.01232787687331438
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.012387661263346672
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.012286301702260971
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.012248095124959946
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.012354159727692604
-#Epoch 9 | Val Loss 0.013362232595682144
-
-
-# 8 stacked
-#Epoch 8 | Val Loss 0.02951284684240818
-#Epoch 9 | Batch 0/9375 | Train Loss 0.031409282237291336
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.027959300205111504
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.028334351256489754
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.027872402220964432
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.028243236243724823
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.028112251311540604
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.028136756271123886
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.02841773070394993
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.028061706572771072
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.027981402352452278
-#Epoch 9 | Val Loss 0.02856108918786049
-
-# Only obf states
-#Epoch 8 | Val Loss 0.03807075321674347
-#Epoch 9 | Batch 0/9375 | Train Loss 0.055063292384147644
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.03324313834309578
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.032573021948337555
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.03292786329984665
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.032870978116989136
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.033228859305381775
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.033142901957035065
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.03274071589112282
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.032672226428985596
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.03245869651436806
-#Epoch 9 | Val Loss 0.03747032582759857
-
-#Epoch 47 | Val Loss 0.02110835164785385
-#Epoch 48 | Batch 0/9375 | Train Loss 0.003424539230763912
-#Epoch 48 | Batch 1000/9375 | Train Loss 0.016024695709347725
-#Epoch 48 | Batch 2000/9375 | Train Loss 0.017232704907655716
-#Epoch 48 | Batch 3000/9375 | Train Loss 0.018149154260754585
-#Epoch 48 | Batch 4000/9375 | Train Loss 0.018040286377072334
-#Epoch 48 | Batch 5000/9375 | Train Loss 0.0181367639452219
-#Epoch 48 | Batch 6000/9375 | Train Loss 0.018189474940299988
-#Epoch 48 | Batch 7000/9375 | Train Loss 0.018370771780610085
-#Epoch 48 | Batch 8000/9375 | Train Loss 0.01825123466551304
-#Epoch 48 | Batch 9000/9375 | Train Loss 0.018198121339082718
-#Epoch 48 | Val Loss 0.021042438223958015
-#Epoch 49 | Batch 0/9375 | Train Loss 0.00415407307446003
-#Epoch 49 | Batch 1000/9375 | Train Loss 0.01748443953692913
-#Epoch 49 | Batch 2000/9375 | Train Loss 0.01810728758573532
-#Epoch 49 | Batch 3000/9375 | Train Loss 0.01810125820338726
-#Epoch 49 | Batch 4000/9375 | Train Loss 0.01812721975147724
-#Epoch 49 | Batch 5000/9375 | Train Loss 0.01800587959587574
-#Epoch 49 | Batch 6000/9375 | Train Loss 0.017994295805692673
-#Epoch 49 | Batch 7000/9375 | Train Loss 0.017940586432814598
-#Epoch 49 | Batch 8000/9375 | Train Loss 0.017944417893886566
-#Epoch 49 | Batch 9000/9375 | Train Loss 0.01823481358587742
-#Epoch 49 | Val Loss 0.02134205587208271
-
-train(conv_regressor, reshape=True)
-
-
-### Validation calculated on obfuscated states
-#Epoch 8 | Val Loss 0.04829911142587662
-#Epoch 9 | Batch 0/9375 | Train Loss 0.005763242021203041
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.014692454598844051
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.014724483713507652
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.014491976238787174
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.014537939801812172
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.014742338098585606
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.014783190563321114
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.014657055027782917
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.014598880894482136
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.014643541537225246
-#Epoch 9 | Val Loss 0.04489811509847641
-
-
-### Validation calculated on all states
-#Epoch 8 | Val Loss 0.02006567269563675
-#Epoch 9 | Batch 0/9375 | Train Loss 0.0059362188912928104
-#Epoch 9 | Batch 1000/9375 | Train Loss 0.015959981828927994
-#Epoch 9 | Batch 2000/9375 | Train Loss 0.015905195847153664
-#Epoch 9 | Batch 3000/9375 | Train Loss 0.016002139076590538
-#Epoch 9 | Batch 4000/9375 | Train Loss 0.01622300036251545
-#Epoch 9 | Batch 5000/9375 | Train Loss 0.016144510358572006
-#Epoch 9 | Batch 6000/9375 | Train Loss 0.016191063448786736
-#Epoch 9 | Batch 7000/9375 | Train Loss 0.016048945486545563
-#Epoch 9 | Batch 8000/9375 | Train Loss 0.015934886410832405
-#Epoch 9 | Batch 9000/9375 | Train Loss 0.015953587368130684
-#Epoch 9 | Val Loss 0.01926545798778534
